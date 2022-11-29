21131b3e18180695eaf1253d53214a8683c7e96b
azizbasaleh <pc.nahdi@gmail.com>
2022-11-29 09:18:46 +0300

    add title

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,12 +8,13 @@ class HomeScreen extends StatelessWidget {
 
   static const String routeName='home';
  //develpoment
+  //ad
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+       title: Text("Hello World"),
       ),
     );
   }
